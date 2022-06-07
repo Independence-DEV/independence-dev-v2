d49cf1d2fb438279a1fed94acdf947f474214a03
arctoken <independence.dev.pro@gmail.com>
2022-06-07 20:09:15 +0300

    favicon and index page

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -9,16 +9,15 @@ export function Welcome() {
       <Title className={classes.title} align="center" mt={100}>
         Welcome to{' '}
         <Text inherit variant="gradient" component="span">
-          Mantine
+          Independence DEV
         </Text>
       </Title>
       <Text color="dimmed" align="center" size="lg" sx={{ maxWidth: 580 }} mx="auto" mt="xl">
-        This starter Next.js project includes a minimal setup for server side rendering, if you want
-        to learn more on Mantine + Next.js integration follow{' '}
-        <Anchor href="https://mantine.dev/theming/next/" size="lg">
-          this guide
+        This website is under construction and will be released soon. You can follow me on{' '}
+        <Anchor href="https://www.youtube.com/c/IndependenceDEV" size="lg">
+          my YouTube channel
         </Anchor>
-        . To get started edit index.tsx file.
+        . Stay tuned!
       </Text>
     </>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,9 +19,9 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   return (
     <>
       <Head>
-        <title>Mantine next example</title>
+        <title>Independence DEV</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.png" />
       </Head>
 
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
